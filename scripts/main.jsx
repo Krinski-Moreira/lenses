3e187e544a70e2624333a57765f0957dbbdba660
unknown <otavioskm@gmail.com>
2023-11-01 10:19:41 -0300

    download button

--- scripts/main.jsx
+++ scripts/main.jsx
@@ -20,10 +20,39 @@ astro.fromFile(fitsFile)
   .catch((error) => {
     console.error('Error reading FITS file:', error);
   });
-Please note that you'll need actual FITS files to test with and work on in your JavaScript application. AstroJS and similar libraries provide methods to read and manipulate FITS files, making it possible to extract information, analyze data, and perform operations on astronomical data stored in these files.
+//Please note that you'll need actual FITS files to test with and work on in your JavaScript application. AstroJS and similar libraries provide methods to read and manipulate FITS files, making it possible to extract information, analyze data, and perform operations on astronomical data stored in these files.
 
-Always refer to the library's documentation or other relevant resources for specific instructions on how to read, extract, and manipulate the data stored in FITS files using the library of your choice, as methods and available functionalities might differ between libraries.
+//Always refer to the library's documentation or other relevant resources for specific instructions on how to read, extract, and manipulate the data stored in FITS files using the library of your choice, as methods and available functionalities might differ between libraries.
 
+// Function to trigger file download
+function downloadFile() {
+  // Content to be included in the downloaded file
+  const fileContent = "This is the content of the file.";
+
+  // Create a Blob with the file content
+  const blob = new Blob([fileContent], { type: 'text/plain' });
+
+  // Create an <a> element
+  const a = document.createElement('a');
+  a.href = URL.createObjectURL(blob);
+  
+  // Set the filename for the downloaded file
+  a.download = 'lensedquasars.fits';
+
+  // Append the <a> element to the document body
+  document.body.appendChild(a);
+
+  // Simulate a click on the <a> element to trigger the download
+  a.click();
+
+  // Clean up: remove the <a> element and the URL object
+  document.body.removeChild(a);
+  URL.revokeObjectURL(a.href);
+}
+
+// Add click event listener to the button
+const downloadButton = document.getElementById('downloadButton');
+downloadButton.addEventListener('click', downloadFile);
 
 
 
