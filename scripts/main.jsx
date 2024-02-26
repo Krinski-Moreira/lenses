0f51075e7a75bc6163e9f83bb0d58cedc1e0a981
unknown <otavioskm@gmail.com>
2024-02-26 16:24:42 -0300

    test1

--- scripts/main.jsx
+++ scripts/main.jsx
@@ -66,3 +66,157 @@ myImage.onclick = () => {
     }
   };
 //favicon-package-v0.16
+
+
+function readCSVFile(){
+     var files = document.querySelector('#file').files;
+
+     if(files.length > 0 ){
+
+          // Selected file
+          var file = files[0];
+
+          // FileReader Object
+          var reader = new FileReader();
+
+          // Read file as string 
+          reader.readAsText(file);
+
+          // Load event
+          reader.onload = function(event) {
+
+               // Read file data
+               var csvdata = event.target.result;
+
+               // Split by line break to gets rows Array
+               var rowData = csvdata.split('\n');
+
+               // <table > <tbody>
+               var tbodyEl = document.getElementById('tblcsvdata').getElementsByTagName('tbody')[0];
+               tbodyEl.innerHTML = "";
+
+               // Loop on the row Array (change row=0 if you also want to read 1st row)
+               for (var row = 1; row < rowData.length; row++) {
+
+                     // Insert a row at the end of table
+                     var newRow = tbodyEl.insertRow();
+
+                     // Split by comma (,) to get column Array
+                     rowColData = rowData[row].split(',');
+
+                     // Loop on the row column Array
+                     for (var col = 0; col < rowColData.length; col++) {
+
+                          // Insert a cell at the end of the row
+                          var newCell = newRow.insertCell();
+                          newCell.innerHTML = rowColData[col];
+
+                     }
+
+               }
+          };
+
+     }else{
+          alert("Please select a file.");
+     }
+
+}
+
+function handleFiles(files) {
+	// Check for the various File API support.
+	if (window.FileReader) {
+		// FileReader are supported.
+		getAsText(files[0]);
+	} else {
+		alert('FileReader are not supported in this browser.');
+	}
+}
+
+function getAsText(fileToRead) {
+	var reader = new FileReader();
+	// Handle errors load
+	reader.onload = loadHandler;
+	reader.onerror = errorHandler;
+	// Read file into memory as UTF-8      
+	reader.readAsText(fileToRead);
+}
+
+function loadHandler(event) {
+	var csv = event.target.result;
+	processData(csv);             
+}
+
+function processData(csv) {
+    var allTextLines = csv.split(/\r\n|\n/);
+    var lines = [];
+    while (allTextLines.length) {
+        lines.push(allTextLines.shift().split(','));
+    }
+	console.log(lines);
+	drawOutput(lines);
+}
+
+//if your csv file contains the column names as the first line
+function processDataAsObj(csv){
+    var allTextLines = csv.split(/\r\n|\n/);
+    var lines = [];
+	
+    //first line of csv
+    var keys = allTextLines.shift().split(',');
+	
+    while (allTextLines.length) {
+        var arr = allTextLines.shift().split(',');
+        var obj = {};
+        for(var i = 0; i < keys.length; i++){
+            obj[keys[i]] = arr[i];
+	}
+        lines.push(obj);
+    }
+        console.log(lines);
+	drawOutputAsObj(lines);
+}
+
+function errorHandler(evt) {
+	if(evt.target.error.name == "NotReadableError") {
+		alert("Canno't read file !");
+	}
+}
+
+function drawOutput(lines){
+	//Clear previous data
+	document.getElementById("output").innerHTML = "";
+	var table = document.createElement("table");
+	for (var i = 0; i < lines.length; i++) {
+		var row = table.insertRow(-1);
+		for (var j = 0; j < lines[i].length; j++) {
+			var firstNameCell = row.insertCell(-1);
+			firstNameCell.appendChild(document.createTextNode(lines[i][j]));
+		}
+	}
+	document.getElementById("output").appendChild(table);
+}
+
+//draw the table, if first line contains heading
+function drawOutputAsObj(lines){
+	//Clear previous data
+	document.getElementById("output").innerHTML = "";
+	var table = document.createElement("table");
+	
+	//for the table headings
+	var tableHeader = table.insertRow(-1);
+ 	Object.keys(lines[0]).forEach(function(key){
+ 		var el = document.createElement("TH");
+		el.innerHTML = key;		
+		tableHeader.appendChild(el);
+	});	
+	
+	//the data
+	for (var i = 0; i < lines.length; i++) {
+		var row = table.insertRow(-1);
+		Object.keys(lines[0]).forEach(function(key){
+			var data = row.insertCell(-1);
+			data.appendChild(document.createTextNode(lines[i][key]));
+		});
+	}
+	document.getElementById("output").appendChild(table);
+}
